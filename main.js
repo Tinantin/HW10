4cb9c179247361477fd3e6348cec727de682c5ae
Valentina <valentinalobodane@gmail.com>
2019-12-22 23:02:16 +0200

    Commit 1 to release

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-<<<<<<< HEAD
 //Test commit 2 to develp branch
 
 //Hotfix commit 1
-=======
 //Test commit 2 to develp branch
 
 //Feature 1 change
 //Feature 1 change 1
->>>>>>> feature_1
+
+
+
+
+
+
+
+
+
+
+
+//Release 1 commit
